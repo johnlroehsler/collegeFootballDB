3f913d63eb8d497076ee8e301392ce2df1c198d2
johnlroehsler <johnlroehsler@gmail.com>
2026-03-06 10:49:18 -0500

    final changes for m2

--- scripts/info.py
+++ scripts/info.py
@@ -3,9 +3,11 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
+# This script is mainly used for checking API usage and status
+
+# Config
 env_path = 'config/keys.env'
 load_dotenv(dotenv_path=env_path)
-
 API_KEY = os.getenv('CFB_API_KEY')
 BASE_URL = "https://api.collegefootballdata.com"
 
--- src/ingestion/test_ingestion.py
+++ src/ingestion/test_ingestion.py
@@ -4,17 +4,14 @@ import requests
 import boto3
 from dotenv import load_dotenv
 
+# Config
 env_path = 'config/keys.env'
 load_dotenv(dotenv_path=env_path)
-
-
 API_KEY = os.getenv('CFB_API_KEY')
 BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 BASE_URL = "https://api.collegefootballdata.com/plays"
 
-# Gets play data from CFB API
 def get_plays(year, week, team):
-    """Fetches play-by-play data from the API."""
     headers = {
         'Authorization': f'Bearer {API_KEY}',
     }
@@ -28,20 +25,28 @@ def get_plays(year, week, team):
     print(f"Fetching data for {team} - Year: {year}, Week: {week}...")
     response = requests.get(BASE_URL, headers=headers, params=params)
     response.raise_for_status()
-    
     data = response.json()
-    
-    print("Response:")
-    print(json.dumps(data, indent=4))
-    
+
     return data
 
+#saves the data locally 
+def save_to_local_sample(data, year, week, team):
+    sample_dir = os.path.join("data", "sample")
+    os.makedirs(sample_dir, exist_ok=True)
+    
+    file_name = f"{team}_plays_y{year}_w{week}.json"
+    file_path = os.path.join(sample_dir, file_name)
+    
+    print(f"Writing local sample to: {file_path}")
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
+    print("Local save complete.")
+
+#uploads to s3
 def upload_to_s3(data, year, week, team):
-    """Uploads JSON data to S3 using the required partition strategy."""
     # Create the S3 client
     s3_client = boto3.client('s3')
-    
-    # The partition strategy
+
     s3_key = f"raw/year={year}/week={week}/{team}_plays.json"
     
     json_data = json.dumps(data)
@@ -64,10 +69,15 @@ def main():
         plays_data = get_plays(test_year, test_week, test_team)
     
         if plays_data:
+            # Saves locally
+            save_to_local_sample(plays_data, test_year, test_week, test_team)
+            
+            # Upload to s3
             upload_to_s3(plays_data, test_year, test_week, test_team)
         else:
             print("No data retrieved from API.")
-            
+    
+    # Error handling
     except Exception as e:
         print(f"An error occurred: {e}")
 
