639ab65f7e1c9ecbf7395dfbe40f52f4a3b391bd
johnlroehsler <johnlroehsler@gmail.com>
2026-03-06 08:27:12 -0500

    test

--- src/ingestion/test_ingestion.py
+++ src/ingestion/test_ingestion.py
@@ -12,6 +12,7 @@ API_KEY = os.getenv('CFB_API_KEY')
 BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 BASE_URL = "https://api.collegefootballdata.com/plays"
 
+# Gets play data from CFB API
 def get_plays(year, week, team):
     """Fetches play-by-play data from the API."""
     headers = {
@@ -27,17 +28,22 @@ def get_plays(year, week, team):
     print(f"Fetching data for {team} - Year: {year}, Week: {week}...")
     response = requests.get(BASE_URL, headers=headers, params=params)
     response.raise_for_status()
-    return response.json()
+    
+    data = response.json()
+    
+    print("Response:")
+    print(json.dumps(data, indent=4))
+    
+    return data
 
 def upload_to_s3(data, year, week, team):
     """Uploads JSON data to S3 using the required partition strategy."""
-    # Initialize the S3 client using boto3
+    # Create the S3 client
     s3_client = boto3.client('s3')
     
-    # Define the partition strategy path (M1 Requirement)
+    # The partition strategy
     s3_key = f"raw/year={year}/week={week}/{team}_plays.json"
     
-    # Convert Python dictionary back to a JSON string for storage
     json_data = json.dumps(data)
     
     print(f"Uploading to S3: s3://{BUCKET_NAME}/{s3_key}")
@@ -55,10 +61,8 @@ def main():
     test_team = "Georgia"
     
     try:
-        # 1. Data Acquisition
         plays_data = get_plays(test_year, test_week, test_team)
-        
-        # 2. Basic Storage 
+    
         if plays_data:
             upload_to_s3(plays_data, test_year, test_week, test_team)
         else:
